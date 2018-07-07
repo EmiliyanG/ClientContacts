8f8693e9c77e27782bb0459ff75df5bd335b004d
EmiliyanG <grigorov.emiliyan@gmail.com>
2018-07-07 13:36:04 +0100

    Change list indents style using left border

---
+++ ClientContactViews/BoolToBorderThicknessConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows;
+
+namespace ClientContactViews
+{
+    public class BoolToBorderThicknessConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+               
+                return (bool)value == true ? new Thickness(0, 0, 0, 0) : new Thickness(5, 0, 0, 0);
+            }
+            else
+            {
+                throw new InvalidOperationException("value should be boolean");
+                
+            }
+
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- ClientContactViews/MarginConverter.cs
+++ ClientContactViews/MarginConverter.cs
@@ -10,18 +10,20 @@ namespace ClientContactViews
 {
     public class MarginConverter : IValueConverter
     {
+        private static double bottomMargin = 5;
+        private static double leftMargin = 15;
 
         public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             if (value == null)
             {
-                return new Thickness(System.Convert.ToDouble(0), 0, 0, 5);
+                return new Thickness(0, 0, 0, bottomMargin);
             }
             else
             {
                 return (String)value == ""
-                                           ? new Thickness(System.Convert.ToDouble(0), 0, 0, 5)
-                                           : new Thickness(System.Convert.ToDouble(20), 0, 0, 5);
+                                           ? new Thickness(0, 0, 0, bottomMargin)
+                                           : new Thickness(leftMargin, 0, 0, bottomMargin);
             }
 
 
