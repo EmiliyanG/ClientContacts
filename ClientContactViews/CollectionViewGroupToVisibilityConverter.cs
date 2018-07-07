3376b22524ad472350202948f4b3efc6ff99a673
EmiliyanG <grigorov.emiliyan@gmail.com>
2018-07-07 21:44:32 +0100

    Add expandable menu to the first level groups (organisation) in the contacts list

---
+++ ClientContactViews/CollectionViewGroupToVisibilityConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ClientContactViews
+{
+
+    public class CollectionViewGroupToVisibilityConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is CollectionViewGroup pgd)
+            {
+                return pgd.IsBottomLevel  == false ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else
+            {
+                throw new InvalidOperationException("expected object of type: CollectionViewGroup");
+            }
+            
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        
+}
+
+}
