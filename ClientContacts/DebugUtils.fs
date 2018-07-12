25b1d0a62853051f92604cb94caf1186f4743726
EmiliyanG <grigorov.emiliyan@gmail.com>
2018-07-12 21:55:50 +0100

    Add debug function to log messages to the Visual Studio output console

---
+++ ClientContacts/DebugUtils.fs
@@ -0,0 +1,14 @@
+﻿namespace ClientContacts
+
+module DebugUtils =
+    open System.Diagnostics
+    open System
+
+    let getTime() = DateTime.Now.ToString()
+    let (^) a b =
+        sprintf "%s%s" a b
+
+    let debug s =
+        
+        Debug.WriteLine(getTime() ^ " > " ^ s)
+
