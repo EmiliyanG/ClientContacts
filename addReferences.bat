f8651496403425813eaf03528d1caa5462c79c03
EmiliyanG <grigorov.emiliyan@gmail.com>
2018-07-21 15:06:06 +0100

    Add System.Windows.Interactivity to paket

--- addReferences.bat
+++ addReferences.bat
@@ -2,3 +2,5 @@
 
 ".paket\paket.exe" add Dapper --project ClientContacts
 ".paket\paket.exe" add Elmish.WPF --project ClientContacts
+".paket\paket.exe" add System.Windows.Interactivity.WPF --project ClientContactViews
+".paket\paket.exe" add System.Windows.Interactivity.WPF --project ClientContacts
