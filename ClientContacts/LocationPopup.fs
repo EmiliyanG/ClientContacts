2d9c5a4504930798d6abb8b0cbc09ddf092cdf3a
EmiliyanG <grigorov.emiliyan@gmail.com>
2018-08-20 23:22:28 +0100

    add custom location popup

---
+++ ClientContacts/LocationPopup.fs
@@ -0,0 +1,25 @@
+﻿namespace ClientContacts
+module LocationPopup=
+    open Elmish
+    open Elmish.WPF
+    open System
+    open SQLTypes
+
+    type Msg = 
+        |UpdateModel
+        
+    type Model = { organisationsList:Organisation seq; locationField:string option}
+
+    let init() = {organisationsList = []; locationField = None}
+    
+    let update msg model = 
+        match msg with
+        |UpdateModel -> model
+    
+
+    let view (msg:Msg) (model:Model) = 
+        [ "location" |> Binding.oneWay (fun m -> "")
+          ]
+
+
+
