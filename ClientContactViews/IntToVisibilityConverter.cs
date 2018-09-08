b626b0c54cf15c60142f7d41a710b02dddb93401
EmiliyanG <grigorov.emiliyan@gmail.com>
2018-09-08 23:18:08 +0100

    Display explanatory text if no organisation contacts are available.

---
+++ ClientContactViews/IntToVisibilityConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using SharedTypes;
+
+namespace ClientContactViews
+{
+    class IntToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                {
+                    return (int)value == 0 ? Visibility.Visible : Visibility.Collapsed;
+                }
+
+            }
+
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
